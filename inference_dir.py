3dff86b168bfc22b5e89e40f77ab64baea163b5b
Theodoros Giannakopoulos <tyiannak@gmail.com>
2023-03-14 20:04:17 +0200

    evaluate dir

--- inference_dir.py
+++ inference_dir.py
@@ -2,28 +2,36 @@ import argparse
 import inference
 import glob
 import os
-import pickle5 as pickle
+import json
+import numpy as np
 
-def inference_dir(dir, model, ground_truth):
+
+def inference_dir(dir, model, ground_truth_file):
     '''
     :param dir: input DIR 
     :param model: model path
     :ground_truth: ground truth json file
     :return: average prediction
     '''
-    model_dict = pickle.load(open(model, 'rb'))
-    regressor = model_dict['model']
-    mid_window = model_dict['mid_window']
-    step = model_dict['mid_step']
-    scaler = model_dict['scaler']
-
-
     files = glob.glob(os.path.join(dir, "*.wav"))
 
-    for f in files:
-        pred = inference.inference(f, model)
-        print(f"y={pred:.2f}")
-
+    with open(ground_truth_file) as fp:
+        ground_truth = json.load(fp)
+    errors = []
+    for f in files[::40]:
+        f2 = os.path.basename(f)
+        found = False
+        for g in ground_truth:
+            if g['name'] == f2:
+                found = True
+                break
+        if found:
+            model_name = model.split("/")[-1].split('.')[0]
+            real = g[model_name]['mean']
+            pred = inference.inference(f, model)
+            errors.append(np.abs(real-pred))
+            print(f"pred={pred:.2f} real={real:.2f}")
+        print(np.mean(errors))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -39,5 +47,5 @@ if __name__ == "__main__":
     flags = parser.parse_args()
     input = flags.input_audio
     model = flags.model_path
-    inference_dir(input, model, "")
+    inference_dir(input, model, "annotations_metadata.json")
 
